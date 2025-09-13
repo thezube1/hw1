8ba7ed34b999f9ca4a4a558d6f9c4ec551a01519
Zubin Hydrie <hydrie@usc.edu>
2025-09-13 04:49:56 +0000

    Added problem 2 test

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,6 +9,63 @@
 
 int main(int argc, char* argv[])
 {
+  ULListStr test;
   
+  std::cout << "Empty initially: " << (test.empty() ? "YES" : "NO") << "\n";
+  std::cout << "Size initially: " << test.size() << "\n";
+  
+  test.push_back("A");
+  test.push_back("B");
+  test.push_back("C");
+  test.push_back("D");
+  test.push_back("E");
+  test.push_back("F");
+
+  std::cout << "Front: " << test.front();
+  std::cout << "Back: " << test.back();
+
+  test.push_front("Z");
+  test.push_front("Y");
+  test.push_front("X");
+  test.push_front("R");
+  test.push_front("Q");
+
+  std::cout << "Size: " << test.size();
+  std::cout << "Front: " << test.front();
+  std::cout << "Back: " << test.back();
+
+  std::cout << "Get (2): " << test.get(2);
+  test.set(2, "A2");
+  std::cout << "Get again (2): " << test.get(2);
+
+  test.pop_front();
+  std::cout << "Front: " << test.front();
+  std::cout << "Size: " << test.size();
+
+  test.pop_back();
+  std::cout << "Back: " << test.back();
+  std::cout << "Size: " << test.size();
+
+  test.pop_back();
+  test.pop_back();
+  test.pop_back();
+  test.pop_back();
+  test.pop_back();
+  test.pop_back();
+  test.pop_back();
+  test.pop_front();
+  test.pop_front();
+  test.pop_front();
+
+  std::cout << "Size: " << test.size();
+
+
+
+
+
+
+
+
+
 
 }
